7e6d352c4f9b1b188c0a6c020a47ddfe9cd66a79
GiuliaCruz <giuliacruzz@gmail.com>
2024-03-04 17:19:31 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ const express = require("express")
 const mongoose = require('mongoose')
 
 const app = express()
+app.use(express.json())
 const port = 3000
-mongoose.connect('mongodb+srv://Thunder1932:6ABZ8DTvIqpp7QNs@starwars-api.cblz8gj.mongodb.net/?retryWrites=true&w=majority&appName=Starwars-API')
-
 
 const Movie = mongoose.model('Movie', {
      title: String,
@@ -13,8 +12,27 @@ const Movie = mongoose.model('Movie', {
      trailer_url: String,
     });
 
-app.get('/', (req, res)=> {
-    res.send("Hello Word")
+app.get('/', async (req, res) => { 
+    const movies = await Movie.find()
+    return res.send(movies)
+})
+
+app.delete("/:id", async(req, res) => {
+    const movie = await Movie.findByIdAndDelete(req.params.id)
+    return res.send(movie)
+})
+
+app.put("/:id", async(req, res) =>{
+    const movie = await Movie.findByIdAndUpdate(req.params.id, {
+        title: req.body.title,
+        description: req.body.description,
+        image_url: req.body.image_url,
+        trailer_url: req.body.trailer_url,
+    }, {
+        new: true //updated information
+    })
+
+    return res.send(movie)
 })
 
 app.post("/", async (req, res) => {
@@ -27,8 +45,9 @@ app.post("/", async (req, res) => {
     })
 
     await movie.save()
-    res.send(movie)
+    return res.send(movie)
 })
 app.listen(port, () =>{
+    mongoose.connect('mongodb+srv://Thunder1932:6ABZ8DTvIqpp7QNs@starwars-api.cblz8gj.mongodb.net/?retryWrites=true&w=majority&appName=Starwars-API')
     console.log('App running')
 })
